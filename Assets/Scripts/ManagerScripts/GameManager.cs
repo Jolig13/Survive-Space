50364ae8593cbde732889e7a2e19d448bf9115c0
Galarza Joel <joelfgalarza97@gmail.com>
2023-11-22 11:27:43 -0300

    commit 3
    Code optimization

--- Assets/Scripts/ManagerScripts/GameManager.cs
+++ Assets/Scripts/ManagerScripts/GameManager.cs
@@ -1,13 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance {get; private set;}
-    [SerializeField] private float score;
+    private float score;
     [SerializeField] private TextMeshProUGUI totalScore;
     [SerializeField] private TextMeshProUGUI highScore;
     private void Awake() 
@@ -23,13 +20,15 @@ public class GameManager : MonoBehaviour
     }
     public void Start() 
     {
-        highScore.text=PlayerPrefs.GetInt("highScore",0).ToString();
+        highScore.text = PlayerPrefs.GetInt("highScore",0).ToString();
     }
     public void Score()
     {
-        score+=1*Time.deltaTime;
-        totalScore.text=((int)score).ToString();
-        if (score>PlayerPrefs.GetInt("hihgScore",0))
+        score += 1*Time.deltaTime;
+
+        totalScore.text = ((int)score).ToString();
+
+        if (score > PlayerPrefs.GetInt("hihgScore",0))
         {
             PlayerPrefs.SetInt("highScore",(int)score);
             highScore.text=score.ToString("0");
